47d7b614de3187cee5c4b741b8aff5cb7acc8762
Nati <nati@flyingcomma.com>
2014-04-09 13:23:48 +0700

    edit database.php

--- app/config/database.php
+++ app/config/database.php
@@ -55,13 +55,13 @@ return array(
 		'mysql' => array(
 			'driver'		=> 'mysql',
 			'host' 		 => $_SERVER['RDS_HOSTNAME'],
-      'port'      => $_SERVER['RDS_PORT'],
-      'database'  => $_SERVER['RDS_DB_NAME'],
-      'username'  => $_SERVER['RDS_USERNAME'],
-      'password'  => $_SERVER['RDS_PASSWORD'],
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
+			'port'      	=> $_SERVER['RDS_PORT'],
+			'database'  	=> $_SERVER['RDS_DB_NAME'],
+			'username'  	=> $_SERVER['RDS_USERNAME'],
+			'password'  	=> $_SERVER['RDS_PASSWORD'],
+			'charset'   	=> 'utf8',
+			'collation' 	=> 'utf8_unicode_ci',
+			'prefix'    	=> '',
 		),
 
 		'pgsql' => array(
